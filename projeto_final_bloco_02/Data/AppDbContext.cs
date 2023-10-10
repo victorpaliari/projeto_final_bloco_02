2a73b864e667fe6ac45fc278165aaa5da3725a07
Victor Paliari <victorrpaliari@gmail.com>
2023-10-10 09:47:21 -0300

    Primeira Etapa

---
+++ projeto_final_bloco_02/Data/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace projeto_final_bloco_02.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+
+    }
+}
---
+++ projeto_final_bloco_02/Program.cs
@@ -0,0 +1,69 @@
+
+using Microsoft.EntityFrameworkCore;
+using projeto_final_bloco_02.Data;
+
+namespace projeto_final_bloco_02
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+
+            // Conexão com o Banco de dados
+            var connectionString = builder.Configuration.
+                    GetConnectionString("DefaultConnection");
+
+            builder.Services.AddDbContext<AppDbContext>(options =>
+                options.UseSqlServer(connectionString)
+            );
+
+
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            // Configuração do CORS
+            builder.Services.AddCors(options => {
+                options.AddPolicy(name: "MyPolicy",
+                    policy =>
+                    {
+                        policy.AllowAnyOrigin()
+                        .AllowAnyHeader()
+                        .AllowAnyMethod();
+                    });
+            });
+
+            var app = builder.Build();
+
+            // Criar o Banco de dados e as tabelas Automaticamente
+            using (var scope = app.Services.CreateAsyncScope())
+            {
+                var dbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+                dbContext.Database.EnsureCreated();
+            }
+
+            app.UseDeveloperExceptionPage();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseCors("MyPolicy");
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
